b2f099040498bba7618c2c97543969e5896e2782
Saksham Sahgal <73836766+SakshamSahgal@users.noreply.github.com>
2021-12-26 15:36:15 +0530

    removing usless folders
    non CP related folders are removed.

--- New folder/avdd.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main()
-{
-    char **names = {"abcd","aaaa"};
-    cout<<names[0];
-    return 0;
-}
--- New folder/pointer3.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    char x = 'a';
-    char *names = {"asda","aaaa","sss"};
-    printf("Value of *ptr = %d\n", *ptr);
-}
-
